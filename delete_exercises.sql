bb7617565ce1f5977f0856448ba9e079ffb5dc2c
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-24 09:55:01 -0500

    Delete exercise complete

---
+++ delete_exercises.sql
@@ -0,0 +1,14 @@
+use codeup_test_db;
+
+select *
+from albums where release_date > 1991;
+
+select *
+from albums where release_date > 1991;
+
+delete from albums where release_date = 1991;
+
+
+select * from albums where artist = 'Nirvana';
+
+delete from albums where artist = 'Nirvana';
