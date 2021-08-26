33a7d935f09d32139bc1d505b0c82c7e929ca4a1
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-26 09:59:46 -0500

    mysql clause Alias exercise complete

---
+++ aliases_exercises.sql
@@ -0,0 +1,13 @@
+use employees;
+
+# Return 10 employees in a result set named 'full_name' in the format of 'last name,
+# first name' for each employee.
+#
+# Add the date of birth for each employee as 'DOB' to the query.
+#
+# Update the query to format full name to include the employee number so
+# it is formatted as 'employee number - last name, first name'.
+
+
+
+select concat(e.emp_no, ' - ' ,e.last_name, ', ', e.first_name, e.birth_date) full_name from employees.employees e limit 10;
--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,8 +1,8 @@
 use employees;
 
 
-Update your queries for employees whose names start and end with 'E'.
-Use concat() to combine their first and last name together as a single column in your results.
+# Update your queries for employees whose names start and end with 'E'.
+# Use concat() to combine their first and last name together as a single column in your results.
 
 
 select concat(first_name, ' ', last_name)
@@ -10,7 +10,7 @@ from employees
 where first_name like 'e%'
   and last_name like '%a';
 
-Find all employees born on Christmas — 842 rows.
+# Find all employees born on Christmas — 842 rows.
 
 select *
 from employees
@@ -18,7 +18,7 @@ where month(birth_date) = 12
   and day(birth_date) = 25;
 
 
-Find all employees hired in the 90s and born on Christmas — 362 rows.
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
 
 
 select *
@@ -29,8 +29,8 @@ where year(hire_date) between 1990 and 1999
 order by hire_date;
 
 
-Change the query for employees hired in the 90s and born on Christmas such that
-the first result is the oldest employee who was hired last. It should be Khun Bernini.
+# Change the query for employees hired in the 90s and born on Christmas such that
+# the first result is the oldest employee who was hired last. It should be Khun Bernini.
 
 select *
 from employees
@@ -40,8 +40,8 @@ where year(hire_date) between 1990 and 1999
 order by employees.employees.hire_date desc;
 
 
-For your query of employees born on Christmas and hired in the 90s,
-use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+# For your query of employees born on Christmas and hired in the 90s,
+# use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
 
 select *, datediff(curdate(), employees.employees.hire_date) total
 from employees
