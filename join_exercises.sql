746153c3617a06b6a50a1725ff0ec4a7074006b8
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-29 16:59:12 -0500

    join-exercise #3 in progress

--- join_exercises.sql
+++ join_exercises.sql
@@ -33,7 +33,17 @@ join employees e on dm.emp_no = e.emp_no;
 
 # Find the current titles of employees
 # currently working in the Customer Service department.
-
+# +--------------------+-------+
+# | title              | Total |
+# +--------------------+-------+
+# | Senior Staff       | 11268 |
+# | Staff              |  3574 |
+# | Senior Engineer    |  1790 |
+# | Engineer           |   627 |
+# | Technique Leader   |   241 |
+# | Assistant Engineer |    68 |
+# | Manager            |     1 |
+# +--------------------+-------+
 select t.title, count(t.emp_no) as total
 from departments d
 inner join dept_emp de on d.dept_no = de.dept_no
@@ -42,3 +52,25 @@ where d.dept_name = 'Customer Service'
 and de.to_date = '9999-01-01'
 and t.to_date = '9999-01-01'
 group by t.title
+
+
+# Find the current salary of all current managers.
+# +--------------------+--------------------+--------+
+# | Department Name    | Department Manager | Salary |
+# +--------------------+--------------------+--------+
+# | Customer Service   | Yuchang Weedman    |  58745 |
+# | Development        | Leon DasSarma      |  74510 |
+# | Finance            | Isamu Legleitner   |  83457 |
+# | Human Resources    | Karsten Sigstam    |  65400 |
+# | Marketing          | Vishwani Minakawa  | 106491 |
+# | Production         | Oscar Ghazalie     |  56654 |
+# | Quality Management | Dung Pesch         |  72876 |
+# | Research           | Hilary Kambil      |  79393 |
+# | Sales              | Hauke Zhang        | 101987 |
+# +--------------------+--------------------+--------+
+
+SELECT dep.dept_name as `Department Name`, concat(e.first_name, ' ', e.last_name) as `Department Manager`, salary as `Salary`
+FROM employees.departments dep
+         JOIN dept_manager dm on dep.dept_no = dm.dept_no
+         join employees e on dm.emp_no = e.emp_no
+         join salaries s on e.emp_no = s.emp_no;
